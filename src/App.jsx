de702a782222480de5abdffbce3910aa1c60e1b2
Gonzalo Espinel <gonzaloespinel@outlook.com>
2024-01-29 14:08:38 -0500

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import VillaLogo from './assets/img/VN_SM_Logo.svg'
 import './index.css'
 
 function App() {
-  const [, ] = useState(0)
+  const [,] = useState(0)
 
   return (
     <>
-     
+      <img src={VillaLogo} alt="Logo Villanueva Olive Oils" />
+
     </>
   )
 }
+
 
 export default App
